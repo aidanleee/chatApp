a7152fef885d0dd45bab59c1de00d43fdfd3d57d
Aidan Lee <Aidan@Aidans-MBP.fc.ca>
2019-03-27 13:23:41 -0400

    add app

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')();
 
 const port = process.env.PORT || 3000;
 
@@ -12,6 +11,29 @@ app.get('/', (req, res ) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
-http.listen(port, () => {
-    console.log(`app is running on port ${port}`);
+
+// create server variable for socket.io to use
+const server = app.listen(port, () => {
+    console.log(`App is running on port ${port}`);
+});
+
+io.attach(server);
+
+// socket.io chat app stuff to follow
+io.on('connection', function(socket) {
+    console.log('a user has connected');
+
+    socket.emit('connected', {sID: `${socket.id}`, message: 'new connection'} );
+
+    // listen for an incoming message from anyone connected to the app
+    socket.on('chat message', function(msg) {
+        console.log('message: ', msg, 'socket', socket.id);
+
+        // send the message to everyone connected to the app
+        io.emit('chat message', { id: `${socket.id}`, message: msg});
+    });
+
+    socket.on('disconnect', function() {
+        console.log(' a user has disconnected');
+    });
 });
